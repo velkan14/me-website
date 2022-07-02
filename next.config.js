ee471243a357b497afe84373061118f8b10b1ccd
danielamado <daniel@precycle.today>
2022-07-02 12:50:06 +0200

    package: add contentlayer

---
+++ next.config.js
@@ -0,0 +1,6 @@
+//import { withContentlayer } from "next-contentlayer";
+const { withContentlayer } = require("next-contentlayer");
+
+//export default withContentlayer({});
+
+module.exports = withContentlayer({});
